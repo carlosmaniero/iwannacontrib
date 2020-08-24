556f68c0911399ccc3701b45015ea57c3da12f10
Maniero <cmaniero@thoughtworks.com>
2020-08-24 15:06:57 -0300

    avoid to execute django heroku on github actions

--- iwannacontrib/settings.py
+++ iwannacontrib/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if not os.environ.get('CI'):
+    django_heroku.settings(locals())
